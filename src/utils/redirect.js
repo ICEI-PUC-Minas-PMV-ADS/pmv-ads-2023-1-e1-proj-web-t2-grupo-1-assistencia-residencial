0f8361931899b4364280b4c8caaafd1e8b0f56cb
Sandro Maciel <sandros.maciel@gmail.com>
2023-06-22 19:56:04 -0300

    teste troca de path

--- src/utils/redirect.js
+++ src/utils/redirect.js
@@ -2,7 +2,7 @@ const defaultPath =
   "pmv-ads-2023-1-e1-proj-web-t2-grupo-1-assistencia-residencial";
 
 function redirectTo(path) {
-  const localDomains = ["localhost", "127.0.0.1", "192.168.x.x", "10.x.x.x"];
+  const localDomains = ["localhost", "127.0.0.1", "192.168.x.x", "10.x.x.x", "https://github.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2023-1-e1-proj-web-t2-grupo-1-assistencia-residencial/blob/main"];
   const localDomain = window.location.hostname;
 
   if (localDomains.some((domain) => domain.includes(localDomain))) {
